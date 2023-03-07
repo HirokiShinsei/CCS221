32c36bf055f560bdaed48ffb07309c93d247b037
HirokiShinsei <matthewandrei.valencia@wvsu.edu.ph>
2023-03-07 20:14:54 +0800

    Activity 2 upload

---
+++ pages/Activity2.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.write('Choose where to plot in a 3x3 grid')
+st.write(' ')
+st.write('1 2 3')
+st.write('4 5 6')
+st.write('7 8 9')
+
+choice = st.selectbox('Choice:', [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+if choice == 1:
+    two_d_arr = np.array([])
+
+    for i in two_d_arr:
+        np.append()
+
+    plt.imshow(two_d_arr, interpolation='none', cmap='winter')
+    st.pyplot()
