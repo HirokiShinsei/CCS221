e4d63522a554e87a927b913913f89d84999e2afb
HirokiShinsei <matthewandrei.valencia@wvsu.edu.ph>
2023-03-13 14:08:42 +0800

    Added Act 3 & 4

---
+++ pages/activity3.py
@@ -0,0 +1,139 @@
+#Group - 6
+#Lord Patrick Raizen Togonon
+#Matthew Andrei Valencia
+#Cyril Reynold Trojillo
+
+import streamlit as st
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+#The following functions are for each specific transformation to be applied to the image.
+def translation(img, tx, ty):
+    rows, cols = img.shape[:2]
+    m_translation_ =np.float32([[1,0, tx],
+                        [0,1 , ty],
+                        [0,0,1]])
+
+    translated_img_= cv2.warpPerspective(img,m_translation_,(cols, rows))
+    return translated_img_
+
+def rotation(img, rotx):
+    angle = np.radians(10)
+    rows, cols = img.shape[:2]
+    m_rotation_ = np.float32([[np.cos(angle), -(np.sin(angle)), 0],
+                        [np.sin(angle), np.cos(angle),0],
+                        [0,0,1]])
+
+    m_rotation_=cv2.getRotationMatrix2D((cols/2,rows/2), rotx, 1)
+    rotated_img_ = cv2.warpAffine(img,m_rotation_,(cols,rows))
+    return rotated_img_
+
+def scaling_img(img, scaleX, scaleY):
+    rows, cols = img.shape[:2]
+    m_scaling_=np.float32([[scaleX ,0 ,0],
+                       [0, scaleY ,0],
+                       [0,0,1]])
+    scaled_img_ =cv2.warpPerspective(img,m_scaling_,(cols*2,rows*2))
+    return scaled_img_
+
+def reflection_v(img):
+    rows, cols = img.shape[:2]
+    m_reflection_ =np.float32([[1,0,0],
+                          [0,-1, rows],
+                          [0,0,1]])
+    reflected_img_ =cv2.warpPerspective(img,m_reflection_,(int(cols),int(rows)))
+    return reflected_img_
+
+def reflection_h(img):
+    rows, cols = img.shape[:2]
+    m_reflection_ =np.float32([[-1, 0, cols],
+                          [0, 1, 0],
+                          [0, 0, 1]])
+    reflected_img_ =cv2.warpPerspective(img,m_reflection_,(int(cols),int(rows)))
+    return reflected_img_
+
+def shear_X(img, shearX):
+    rows, cols = img.shape[:2]
+    m_shearing_x=np.float32([[1, shearX ,0],
+                         [0,1,0],
+                         [0,0,1]])
+
+    sheared_img_x = cv2.warpPerspective(img, m_shearing_x,(int(cols*1.5),int(rows*1.5)))
+    return sheared_img_x
+
+def shear_Y(img, shearY):
+    rows, cols = img.shape[:2]
+    m_shearing_x=np.float32([[1, 0 ,0],
+                         [shearY, 1 ,0],
+                         [0, 0, 1]])
+
+    sheared_img_x = cv2.warpPerspective(img, m_shearing_x,(int(cols*1.5),int(rows*1.5)))
+    return sheared_img_x
+
+def main():
+    #The following lines takes user input for shifting X, Y, and rotation directions, shear value(skew) whether it be to the right or left.
+    tx = st.slider("Enter the value to shift in X-axis", -100, 100, 0)
+	ty = st.slider("Enter the value to shift in Y-axis", -100, 100, 0)
+	rotx = st.slider("Enter value to rotate the image in degrees", -180, 180, 0)
+	scaleX = st.slider("Enter the value to scale in X-Axis", 0.1, 5.0, 1.0)
+	scaleY = st.slider("Enter the value to scale in Y-Axis", 0.1, 5.0, 1.0)
+	shearX = st.slider("Enter the value to shear in X-Axis", -1.0, 1.0, 0.0, step=0.1)
+	shearY = st.slider("Enter the value to shear in Y-Axis", -1.0, 1.0, 0.0, step=0.1)
+
+for i in range(1, 6): # For loop to read a specific number of images to apply the transformations on.
+    img_path = f"image{i}.jpg"
+    img = cv2.imread(img_path)
+    cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    #The following lines calls each of the functions for specific transformations of the images.
+    translated_img_ = translation(img, tx, ty)
+    rotated_img_ = rotation(img, rotx)
+    scaled_img_ = scaling_img(img, scaleX, scaleY)
+    reflected_h = reflection_h(img)
+    reflected_v = reflection_v(img)
+    sheared_img_x = shear_X(img, shearX)
+    sheared_img_y = shear_Y(img, shearY)
+
+    #The following functions plots where the specific transformation be placed on a 2 column by 4 rows figure.
+    fig, axs = plt.subplots(2, 4, figsize=(18, 14))
+    fig = plt.gcf()
+    fig.canvas.manager.set_window_title('OpenCV Transformations') #This line sets the window title to OpenCV Transformations
+
+    axs[0, 0].imshow(img)
+    axs[0, 0].set_title("Original Image")
+    axs[0, 0].axis("off")
+
+    axs[0, 1].imshow(translated_img_)
+    axs[0, 1].set_title("Translated Image")
+    axs[0, 1].axis("off")
+
+    axs[0, 2].imshow(rotated_img_)
+    axs[0, 2].set_title("Rotated Image")
+    axs[0, 2].axis("off")
+
+    axs[0, 3].imshow(scaled_img_)
+    axs[0, 3].set_title("Scaled Image")
+    axs[0, 3].axis("off")
+
+    axs[1, 0].imshow(reflected_h)
+    axs[1, 0].set_title("Reflected Image (Horizontal)")
+    axs[1, 0].axis("off")
+
+    axs[1, 1].imshow(reflected_v)
+    axs[1, 1].set_title("Reflected Image (Vertical)")
+    axs[1, 1].axis("off")
+
+    axs[1, 2].imshow(sheared_img_x)
+    axs[1, 2].set_title("Sheared Horizontally(X)")
+    axs[1, 2].axis("off")
+
+    axs[1, 3].imshow(sheared_img_y)
+    axs[1, 3].set_title("Sheared Vertically(Y)")
+    axs[1, 3].axis("off")
+
+    st.pyplot(fig)
+
+
+
+
---
+++ pages/activty4.py
@@ -0,0 +1,155 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from scipy.spatial import Delaunay
+import tensorflow as tf
+
+tf.compat.v1.disable_eager_execution()
+
+def _plt_basic_object_(points):
+    tri = Delaunay(points).convex_hull
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    s = ax.plot_trisurf(points[:, 0], points[:, 1], points[:, 2],
+                         triangles=tri,
+                         shade=True, cmap=cm.rainbow, lw=0.5)
+    ax.set_xlim3d(-5, 5)
+    ax.set_ylim3d(-5, 5)
+    ax.set_zlim3d(-5, 5)
+    st.pyplot(fig)
+
+def _cube_(bottom_lower=(0, 0, 0), side_length=5):
+    bottom_lower = np.array(bottom_lower)
+    points = np.vstack([
+        bottom_lower,
+        bottom_lower + [0, side_length, 0],
+        bottom_lower + [side_length, side_length, 0],
+        bottom_lower + [side_length, 0, 0],
+        bottom_lower + [0, 0, side_length],
+        bottom_lower + [0, side_length, side_length],
+        bottom_lower + [side_length, side_length, side_length],
+        bottom_lower + [side_length, 0, side_length],
+        bottom_lower,
+    ])
+    return points
+
+def _triangular_prism_(bottom_lower=(0, 0, 0), side_length=5, height=5):
+    bottom_lower = np.array(bottom_lower)
+    points = np.vstack([
+        bottom_lower,
+        bottom_lower + [0, side_length, 0],
+        bottom_lower + [side_length, side_length, 0],
+        bottom_lower + [0, 0, height],
+        bottom_lower + [0, side_length, height],
+        bottom_lower + [side_length, side_length, height],
+        bottom_lower,
+    ])
+    rotation_matrix = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
+    points = np.dot(points, rotation_matrix.T)
+    return points
+
+def _pyramid_(bottom_lower=(0, 0, 0), side_length=5, height=5):
+    bottom_lower = np.array(bottom_lower)
+    base_points = np.vstack([
+        bottom_lower,
+        bottom_lower + [0, side_length, 0],
+        bottom_lower + [side_length, side_length, 0],
+        bottom_lower + [side_length, 0, 0],
+    ])
+    top_point = bottom_lower + [side_length / 2, side_length / 2, height]
+    points = np.vstack([base_points, top_point])
+    return points
+
+def _rectangular_prism_(bottom_lower=(0, 0, 0), length=5, width=5, height=5):
+    
+    bottom_lower = np.array(bottom_lower)
+    
+    points = np.vstack([
+        bottom_lower,
+        bottom_lower + [0, width, 0], 
+        bottom_lower + [length, width, 0], 
+        bottom_lower + [length, 0, 0],
+        bottom_lower + [0, 0, height],
+        bottom_lower + [0, width, height],
+        bottom_lower + [length, width, height],
+	bottom_lower + [length, 0, height],
+	bottom_lower,
+])
+
+    return points
+
+  def translate_obj(points, amount):
+    return tf.add(points, amount)
+
+init_cube = cube(side_length=5)
+points_cube = tf.constant(init_cube, dtype=tf.float32)
+
+init_triangular_prism = triangular_prism(side_length=5, height=5)
+points = tf.constant(init_triangular_prism, dtype=tf.float32)
+
+init_pyramid = pyramid(side_length=5, height=5)
+points2 = tf.constant(init_pyramid, dtype=tf.float32)
+
+init_rectangular_prism = rectangular_prism(length=5, width=3, height=4)
+points3 = tf.constant(init_rectangular_prism, dtype=tf.float32)
+
+translation_amount = tf.constant([1, 2, 3], dtype=tf.float32)
+translated_object = translate_obj(points, translation_amount)
+translated_object2 = translate_obj(points2, translation_amount)
+translated_object3 = translate_obj(points_cube, translation_amount)
+translated_object4 = translate_obj(points3, translation_amount)
+
+with tf.compat.v1.Session() as session:
+translated_triangular_prism = session.run(translated_object)
+
+with tf.compat.v1.Session() as session:
+translated_pyramid = session.run(translated_object2)
+
+with tf.compat.v1.Session() as session:
+translated_cube = session.run(translated_object3)
+
+with tf.compat.v1.Session() as session:
+translated_rectangular_prism = session.run(translated_object4)
+
+st.title("3D Object Translation")
+st.header("Original and Translated Objects")
+st.write("Use the sliders to translate the objects in the x, y, and z directions.")
+
+create sliders to adjust translation amount
+translation_x = st.slider("X", -5.0, 5.0, 1.0, 0.1)
+translation_y = st.slider("Y", -5.0, 5.0, 2.0, 0.1)
+translation_z = st.slider("Z", -5.0, 5.0, 3.0, 0.1)
+
+create translated objects
+translation_amount = tf.constant([translation_x, translation_y, translation_z], dtype=tf.float32)
+translated_triangular_prism = translate_obj(points, translation_amount)
+translated_pyramid = translate_obj(points2, translation_amount)
+translated_cube = translate_obj(points_cube, translation_amount)
+translated_rectangular_prism = translate_obj(points3, translation_amount)
+
+run TensorFlow session to get translated objects
+with tf.compat.v1.Session() as session:
+translated_triangular_prism = session.run(translated_triangular_prism)
+translated_pyramid = session.run(translated_pyramid)
+translated_cube = session.run(translated_cube)
+translated_rectangular_prism = session.run(translated_rectangular_prism)
+
+plot original and translated objects
+st.write("Triangular Prism")
+plt_basic_object(init_triangular_prism)
+plt_basic_object(translated_triangular_prism)
+
+st.write("Pyramid")
+plt_basic_object(init_pyramid)
+plt_basic_object(translated_pyramid)
+
+st.write("Cube")
+plt_basic_object(init_cube)
+plt_basic_object(translated_cube)
+
+st.write("Rectangular Prism")
+plt_basic_object(init_rectangular_prism)
+plt_basic_object(translated_rectangular_prism)
+
