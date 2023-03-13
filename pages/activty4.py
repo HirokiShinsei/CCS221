c80ec8f75f05614fb29b3e0d8a0cbd1a9a174417
HirokiShinsei <matthewandrei.valencia@wvsu.edu.ph>
2023-03-13 21:37:35 +0800

    FIxed the misnaming and indentions of functions

--- pages/activty4.py
+++ pages/activty4.py
@@ -14,8 +14,8 @@ def _plt_basic_object_(points):
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(111, projection='3d')
     s = ax.plot_trisurf(points[:, 0], points[:, 1], points[:, 2],
-                         triangles=tri,
-                         shade=True, cmap=cm.rainbow, lw=0.5)
+                        triangles=tri,
+                        shade=True, cmap=cm.rainbow, lw=0.5)
     ax.set_xlim3d(-5, 5)
     ax.set_ylim3d(-5, 5)
     ax.set_zlim3d(-5, 5)
@@ -79,25 +79,27 @@ def _rectangular_prism_(bottom_lower=(0, 0, 0), length=5, width=5, height=5):
         bottom_lower + [0, 0, height],
         bottom_lower + [0, width, height],
         bottom_lower + [length, width, height],
-	bottom_lower + [length, 0, height],
-	bottom_lower,
-])
+        bottom_lower + [length, 0, height],
+        bottom_lower,
+    ])
 
     return points
 
-  def translate_obj(points, amount):
+
+def translate_obj(points, amount):
     return tf.add(points, amount)
 
-init_cube = cube(side_length=5)
+
+init_cube = _cube_(side_length=5)
 points_cube = tf.constant(init_cube, dtype=tf.float32)
 
-init_triangular_prism = triangular_prism(side_length=5, height=5)
+init_triangular_prism = _triangular_prism_(side_length=5, height=5)
 points = tf.constant(init_triangular_prism, dtype=tf.float32)
 
-init_pyramid = pyramid(side_length=5, height=5)
+init_pyramid = _pyramid_(side_length=5, height=5)
 points2 = tf.constant(init_pyramid, dtype=tf.float32)
 
-init_rectangular_prism = rectangular_prism(length=5, width=3, height=4)
+init_rectangular_prism = _rectangular_prism_(length=5, width=3, height=4)
 points3 = tf.constant(init_rectangular_prism, dtype=tf.float32)
 
 translation_amount = tf.constant([1, 2, 3], dtype=tf.float32)
@@ -107,54 +109,54 @@ translated_object3 = translate_obj(points_cube, translation_amount)
 translated_object4 = translate_obj(points3, translation_amount)
 
 with tf.compat.v1.Session() as session:
-translated_triangular_prism = session.run(translated_object)
+    translated_triangular_prism = session.run(translated_object)
 
 with tf.compat.v1.Session() as session:
-translated_pyramid = session.run(translated_object2)
+    translated_pyramid = session.run(translated_object2)
 
 with tf.compat.v1.Session() as session:
-translated_cube = session.run(translated_object3)
+    translated_cube = session.run(translated_object3)
 
 with tf.compat.v1.Session() as session:
-translated_rectangular_prism = session.run(translated_object4)
+    translated_rectangular_prism = session.run(translated_object4)
 
 st.title("3D Object Translation")
 st.header("Original and Translated Objects")
 st.write("Use the sliders to translate the objects in the x, y, and z directions.")
 
-create sliders to adjust translation amount
+# create sliders to adjust translation amount
 translation_x = st.slider("X", -5.0, 5.0, 1.0, 0.1)
 translation_y = st.slider("Y", -5.0, 5.0, 2.0, 0.1)
 translation_z = st.slider("Z", -5.0, 5.0, 3.0, 0.1)
 
-create translated objects
-translation_amount = tf.constant([translation_x, translation_y, translation_z], dtype=tf.float32)
+# create translated objects
+translation_amount = tf.constant(
+    [translation_x, translation_y, translation_z], dtype=tf.float32)
 translated_triangular_prism = translate_obj(points, translation_amount)
 translated_pyramid = translate_obj(points2, translation_amount)
 translated_cube = translate_obj(points_cube, translation_amount)
 translated_rectangular_prism = translate_obj(points3, translation_amount)
 
-run TensorFlow session to get translated objects
+# run TensorFlow session to get translated objects
 with tf.compat.v1.Session() as session:
-translated_triangular_prism = session.run(translated_triangular_prism)
-translated_pyramid = session.run(translated_pyramid)
-translated_cube = session.run(translated_cube)
-translated_rectangular_prism = session.run(translated_rectangular_prism)
+    translated_triangular_prism = session.run(translated_triangular_prism)
+    translated_pyramid = session.run(translated_pyramid)
+    translated_cube = session.run(translated_cube)
+    translated_rectangular_prism = session.run(translated_rectangular_prism)
 
-plot original and translated objects
+# plot original and translated objects
 st.write("Triangular Prism")
-plt_basic_object(init_triangular_prism)
-plt_basic_object(translated_triangular_prism)
+_plt_basic_object_(init_triangular_prism)
+_plt_basic_object_(translated_triangular_prism)
 
 st.write("Pyramid")
-plt_basic_object(init_pyramid)
-plt_basic_object(translated_pyramid)
+_plt_basic_object_(init_pyramid)
+_plt_basic_object_(translated_pyramid)
 
 st.write("Cube")
-plt_basic_object(init_cube)
-plt_basic_object(translated_cube)
+_plt_basic_object_(init_cube)
+_plt_basic_object_(translated_cube)
 
 st.write("Rectangular Prism")
-plt_basic_object(init_rectangular_prism)
-plt_basic_object(translated_rectangular_prism)
-
+_plt_basic_object_(init_rectangular_prism)
+_plt_basic_object_(translated_rectangular_prism)
